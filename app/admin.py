8472a7ad9a916bce9f642260d4b0f12331026110
Kevlxrd <ke.albanez@duocuc.cl>
2024-10-17 11:49:43 -0300

    Moved 'app' folder to 'Intranet'

--- app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuarios, Persona, Horario
-
-# Registrar el modelo Usuarios en el panel de administración
-admin.site.register(Usuarios)
-
-admin.site.register(Persona)
-admin.site.register(Horario)
